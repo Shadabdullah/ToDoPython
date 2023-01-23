f2f8830f9d6cf76b3b3be63a7196d35d73f2ec0a
Shadabdullah <shadabdullah101@gmail.com>
2023-01-23 19:40:15 +0530

    Used txt file to store data

--- Demo.py
+++ Demo.py
@@ -1,13 +1,33 @@
 
-todo = []
+
 
 while True:
     user_input = input("Enter add or Show: ")
 
     match user_input:
         case "add":
-            tod = input("Enter a Todo: ")
-            todo.append(tod)
-        case "show":
-            print(todo)
+            tod = input("Enter a Todo: ")+"\n"
+            file = open("todo.txt",'r')
+            todos = file.readlines()
+            file.close()
 
+            todos.append(tod)
+
+            file = open('todo.txt','w')
+            data = file.writelines(todos)
+            file.close()
+
+
+        case "show":
+            file = open("todo.txt",'r')
+            todos = file.readlines()
+            for ind , item in enumerate (todos):
+                print(f"{ind+1} : { item}")
+
+        case "delete" :
+            pass
+
+        case "edit" :
+            pass
+        case _ :
+            pass

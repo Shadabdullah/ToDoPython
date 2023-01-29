4a7606bc68c91f5320c1007060c272d6222f8e0e
Shadabdullah <shadabdullah101@gmail.com>
2023-01-29 10:57:33 +0530

    Completed

--- Bonus.py
+++ Bonus.py
@@ -1,17 +1,31 @@
-from typing import List
+# from typing import List
+#
+# file = open("todo.txt", 'w')
+#
+# file.write("100.12")
+# file.close()
+# data = open('todo.txt','r')
+# data.close()
+# file = open('todo.txt','w')
+# data.append("111.23"+'\n')
+# file.write()
+#
+# file.close()
+#
+# ##  Working with json
 
-file = open("todo.txt", 'w')
+import PySimpleGUI as sg
 
-file.write("100.12")
-file.close()
-data = open('todo.txt','r')
-data.close()
-file = open('todo.txt','w')
-data.append("111.23"+'\n')
-file.write()
+text_label = sg.Text("Select files to compress")
+input_file = sg.Input()
+inputButton1 = sg.FileBrowse("choose files")
+# -- second row
 
-file.close()
+text_label2 = sg.Text("Choose destination folder")
+destination = sg.InputText()
+outputButton2 =  sg.FolderBrowse(" choose destination")
 
-##  Working with json
+compress = sg.Button('Compress')
+window = sg.Window("zipper app " ,layout=[[text_label , input_file,inputButton1],[text_label2,destination, outputButton2],[compress]])
 
-
+window.read()
--- GUI.py
+++ GUI.py
@@ -1,10 +1,35 @@
 import Function
 import PySimpleGUI as sg
 
-label = sg.Text('Type in todo list')
-input_box = sg.InputText(tooltip="Enter todo")
+label = sg.Text('Type in todo list',font=('monospace',13))
+input_box = sg.InputText(tooltip="Enter todo" ,key="todo" ,font=('Halvatica',13))
+button1 = sg.Button("ADD",font=('Halvatica',13))
 
-window = sg.Window("My To-DO App",layout=[[label],[input_box]])
-window.read()
+todos_list = sg.Listbox(values=Function.get_todos() ,key="todos" ,enable_events=True, size=[55,8])
+list_edit = sg.Button("Edit")
+
+window = sg.Window("My To-DO App",layout=[[label],[input_box ,button1] , [todos_list , list_edit]])
+
+
+
+while True :
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event :
+        case  "ADD":
+            values = values["todo"] + '\n'
+            todos = Function.get_todos()
+            todos.append(values)
+            addTodos = Function.write_todos(todos)
+            Function.get_todos()
+
+        case" Edit":
+            todo_to_edit = values["todos"][0]
+            print(todo_to_edit)
+        case sg.WINDOW_CLOSED:
+            break
+
+
 
 window.close()
